12a6f4dabdfa9f605f4a12eba53ea3abddc5a878
Andrey Akimov <shirase55@gmail.com>
2021-09-01 15:38:28 +0300

    common_hardware

--- src/main/target/common_hardware.c
+++ src/main/target/common_hardware.c
@@ -61,13 +61,10 @@
         #endif
     #endif
 
-    #if defined(USE_IMU_MPU9250) || defined(USE_MAG_MPU9250)
+    #if defined(USE_IMU_MPU9250)
         #if defined(MPU9250_SPI_BUS)
         BUSDEV_REGISTER_SPI(busdev_mpu9250,     DEVHW_MPU9250,      MPU9250_SPI_BUS,    MPU9250_CS_PIN,     GYRO_INT_EXTI,  DEVFLAGS_NONE,  IMU_MPU9250_ALIGN);
-        #elif defined(MPU9250_I2C_BUS)|| defined(MAG_I2C_BUS)
-        #if !defined(MPU9250_I2C_BUS)
-            #define MPU9250_I2C_BUS MAG_I2C_BUS
-        #endif
+        #elif defined(MPU9250_I2C_BUS)
         BUSDEV_REGISTER_I2C(busdev_mpu9250,     DEVHW_MPU9250,      MPU9250_I2C_BUS,    MPU_ADDRESS,        GYRO_INT_EXTI,  DEVFLAGS_NONE,  IMU_MPU9250_ALIGN);
         #endif
     #endif
@@ -203,10 +200,7 @@
 #if defined(USE_MAG_MPU9250)
     #if defined(MPU9250_SPI_BUS)
     BUSDEV_REGISTER_SPI(busdev_mpu9250,      DEVHW_MPU9250,       MPU9250_SPI_BUS,     MPU9250_CS_PIN,      NONE,           DEVFLAGS_NONE,  0);
-    #elif defined(MPU9250_I2C_BUS) || defined(MAG_I2C_BUS)
-    #if !defined(MPU9250_I2C_BUS)
-        #define MPU9250_I2C_BUS MAG_I2C_BUS
-    #endif
+    #elif defined(MPU9250_I2C_BUS)
     BUSDEV_REGISTER_I2C(busdev_mpu9250,      DEVHW_MPU9250,       MPU9250_I2C_BUS,     0x69,               NONE,           DEVFLAGS_NONE,  0);
     #endif
 #endif
